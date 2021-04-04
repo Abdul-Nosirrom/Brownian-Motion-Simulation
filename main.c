d393fd97dbee6b5403cd25a3458ad72db2989c01
Abdul-Nosirrom <abdulal@bu.edu>
2021-04-04 05:39:40 +0300

    Added user input to determine number of particles

--- main.c
+++ main.c
@@ -34,35 +34,38 @@ void reshape(int w, int h)
 
 int main(int argc, char *argv[])
 {
+    int numSpheres;
+    printf("Input the number of particles:  ");
+    scanf("%d", &numSpheres);
 
-  allSpheres = initializeSphere(1000);
+    allSpheres = initializeSphere(numSpheres);
   /* 
    * Using GL Utilities (glut) 
    *
    */
-  glutInit(&argc, argv);                        // Initializes the GL Utilities
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);  // Display mode takes parameters (Using parameters defined as
+    glutInit(&argc, argv);                        // Initializes the GL Utilities
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);  // Display mode takes parameters (Using parameters defined as
                                                 // masks, hence the bitwise or | ) that specificy the mode
                                                 // Here, it's using RBG and DOUBLE (buffering) defined in GLUT
-  glutInitWindowSize(1000,500);
-  glutInitWindowPosition(50,50);
-  glutCreateWindow(argv[0]);                    // 
-  glClearColor(0.0, 0.0, 0.0, 1.0);
-  glMatrixMode(GL_PROJECTION);                  // Specifying that we will use projection matrix [x,y,z,w]
-  glLoadIdentity();                             //
-  gluOrtho2D(-LENGTH, LENGTH, -HEIGHT, HEIGHT); // Puts in the projection matrix an orthogonal projection of 
+    glutInitWindowSize(1000,500);
+    glutInitWindowPosition(50,50);
+    glutCreateWindow(argv[0]);                    // 
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glMatrixMode(GL_PROJECTION);                  // Specifying that we will use projection matrix [x,y,z,w]
+    glLoadIdentity();                             //
+    gluOrtho2D(-LENGTH, LENGTH, -HEIGHT, HEIGHT); // Puts in the projection matrix an orthogonal projection of 
                                                 // specified parameters
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();  
-  glDisable(GL_LIGHTING);                       // Disable because 2D
-  glEnable(GL_LINE_SMOOTH);                     // Basically anti-aliasing?
-  glLineWidth(3.0);
-  glutReshapeFunc(reshape);                     // Two below basically function overloading? but non-oop?
-  glutDisplayFunc(display);
-  glutIdleFunc(display);  
-  glutMainLoop();                               // You know this from working with UI on the alarm clock
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();  
+    glDisable(GL_LIGHTING);                       // Disable because 2D
+    glEnable(GL_LINE_SMOOTH);                     // Basically anti-aliasing?
+    glLineWidth(3.0);
+    glutReshapeFunc(reshape);                     // Two below basically function overloading? but non-oop?
+    glutDisplayFunc(display);
+    glutIdleFunc(display);  
+    glutMainLoop();                               // You know this from working with UI on the alarm clock
 
-  free(allSpheres);
-  allSpheres = NULL;
-  return 0;   
+    free(allSpheres);
+    allSpheres = NULL;
+    return 0;   
 }
